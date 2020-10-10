0a83e844273cb61022ec60b23a3044a046be8af0
Tazwar <tazwar9t6@gmail.com>
2020-10-10 18:50:28 +0600

    added testimonials

--- src/components/MakeAppointments/MakeAppointments.js
+++ src/components/MakeAppointments/MakeAppointments.js
@@ -12,10 +12,10 @@ const MakeAppointments = () => {
                         <img src="https://i.ibb.co/x2S8VwC/doctor.png" alt=""/>
                     </div>
                     <div className="col-md-7 text-white py-5">
-                        <h5 className="text-primary text-uppercase ">Appointment</h5>
+                        <h5 style={{color:"#1cc7c1"}} className=" text-uppercase ">Appointment</h5>
                         <h1 className="my-4">Make an Appointment <br/> Today</h1>
                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque magnam ad consequuntur assumenda saepe hic amet nemo ea facere a!</p>
-                        <button className="btn btn-primary">Learn More</button>
+                        <button className="btn btn-main text-white">Learn More</button>
                     </div>
                 </div>
             </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header/Header';
 import MakeAppointments from '../components/MakeAppointments/MakeAppointments';
 import Services from '../components/Services/Services';
+import Testimonials from '../components/Testimonials/Testimonials';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Header/>
             <Services/>
             <MakeAppointments/>
+            <Testimonials/>
         </div>
     );
 };
